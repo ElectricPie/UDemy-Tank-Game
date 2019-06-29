832ae37b5f42b21b0c0166103ec01197f9a022ca
Connor Priest <Kon_Priest@Hotmail.com>
2019-06-29 14:22:59 +0100

    Added Possession Check
    Tank controller checks if it is possessing a tank at the beginning of play and debugs if its or not to the console.

--- Source/TankGame/Private/TankPlayerController.cpp
+++ Source/TankGame/Private/TankPlayerController.cpp
@@ -2,6 +2,24 @@
 
 #include "TankPlayerController.h"
 
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto controlledTank = GetControlledTank();
+
+	//Checks that the player is possesing a tank
+	if (!controlledTank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player controller not posessing a tank"));
+	}
+	else 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player controller is possesing: %s"), *(controlledTank->GetName()));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const 
 {
 	return Cast<ATank>(GetPawn());
